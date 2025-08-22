176683e87b8a93711d3d669780ca8888fa229d43
hahaha996 <hahaha996>
2025-08-22 13:53:55 +0100

    data of Tempelhof-Schöneberg

--- testweatherwarning.py
+++ testweatherwarning.py
@@ -1,6 +1,7 @@
 import requests, datetime as dt
 
 URL = "https://s3.eu-central-1.amazonaws.com/app-prod-static.warnwetter.de/v16/gemeinde_warnings_v2.json"
+ #nina	(3)[ "110070007007", "Tempelhof-Schöneberg", "Stadt-/Ortsteil bzw. Stadtbezirk" ]
 j = requests.get(URL, timeout=15).json()
 print("test")
 print(j)
@@ -10,3 +11,4 @@ for w in berlin:
         return dt.datetime.fromtimestamp(ms/1000).strftime("%Y-%m-%d %H:%M")
     print(f"[{w.get('level')}] {w.get('event')}  {ts(w['start'])} ? {ts(w['end'])}")
     print(w.get("description","").strip(), "\n")
+s
